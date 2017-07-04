6e4f419b1d27d861aeb046fdb38214ca2653c601
PC201607121100\Administrator <ycycorona@qq.com>
2017-07-04 16:38:57 +0800

    在 server文件夹里简历package.json  ，从而可以把服务端的包维护在自身文件夹中

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 /**
  * Created by ycy42 on 2017/6/15.
  */
-let express = require('../node_modules/express');
-let ejs = require('../node_modules/ejs');
+let express = require('express');
+let ejs = require('ejs');
 let path = require('path');
 let app = express();
+let user = require('./control/user');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,7 +16,7 @@ app.set('view engine', 'html');// app.set('view engine', 'ejs');
 app.locals.title = 'vue-test-project';
 /*app.set('view engine', 'html');*/
 app.use(express.static('public'));
-/*app.use(express.static('views'));*/
+app.use(express.static('views'));
 
 app.use(function(err,req, res, next) {
   res.status(err.status || 500);
@@ -31,17 +32,17 @@ app.use(function(req, res, next) {
 });
 app.use(function(req, res, next) {
   console.log("34");
-});
-/*app.use(function (req, res, next) {
-  console.log('Time: %d', Date.now());
   next();
-});*/
+});
+app.use('/user',user);
 
-/*app.get('/', function (req, res) {
-  res.sendfile('index.html');
-});*/
+app.get('/', function (req, res) {
+  res.sendfile('/index.html');
+});
 
-//静态资源目录
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
